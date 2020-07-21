7a2808eea462dd6972ad8af4d95083c015669513
INE-02 <INE-02@Admins-iMac-5.local>
2020-07-21 17:31:07 +0700

    catchupweek2

---
+++ comment.py
@@ -0,0 +1,6 @@
+"""
+    this is the comments
+    pythons interpreter ignore
+    All the text here
+"""
+print('Hello thathai')    
---
+++ multiple.py
@@ -0,0 +1,8 @@
+name = input ( 'what is your name?')
+age = int(input('what is your age?'))
+income= float(input('what is your income?'))
+#display the data.
+print('here is the data you entered:')
+print('name:', name)
+print('Age:',age)
+print('Income:',format(income,'12,.2f'))
