6ebc6fb140ef7d3a76a31349010d3573e4c454a1
INE-02 <INE-02@Admins-iMac-5.local>
2020-07-21 17:06:49 +0700

    weeek2

---
+++ input.py
@@ -0,0 +1,6 @@
+# get the user's first name.
+firstname = input ('Enter your first name: ')
+#get the user's last name.
+lastname = input('Enter your last name: ')
+#print a greeting to the user.
+print('MR. ',firstname,lastname)
---
+++ membershipweek2.py
@@ -0,0 +1,10 @@
+x= 'hello world'
+y= {1:'a',2:'b'}
+#output : true
+print('h'in x)
+#output:true
+print('hello'not in x)
+#outout : true
+print(1 in y)
+#output : false
+print('a'in y)
