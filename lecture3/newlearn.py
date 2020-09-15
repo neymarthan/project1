944c074d502295d15ad8e1f1dda75296122bc0ce
neymarthan <68272581+neymarthan@users.noreply.github.com>
2020-09-15 12:52:33 +0700

    learnfile.py

---
+++ lecture3/newlearn.py
@@ -0,0 +1,13 @@
+def main():
+    texas()
+    chile()
+
+ def texas():
+     birds=5000
+     print('texas has',birds,'birds.') 
+
+def chile():
+    birds=8000
+    print('chile has',birds,'birds.') 
+
+main()          
